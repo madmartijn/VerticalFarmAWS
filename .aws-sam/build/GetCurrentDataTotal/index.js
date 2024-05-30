5ba5822987ae19a28abefa6dfbd4ea420a06e409
MARTIJN-DESKTOP\madma <mpmj.vanzelst@student.avans.nl>
2024-05-30 11:52:37 +0200

    Fixed api online

--- .aws-sam/build/GetCurrentDataTotal/index.js
+++ .aws-sam/build/GetCurrentDataTotal/index.js
@@ -2,5 +2,5 @@ exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  return { statusCode: 200, body: JSON.stringify({ message: 'Hello World' }) };
 };
--- src/GetCurrentDataTotal/index.js
+++ src/GetCurrentDataTotal/index.js
@@ -1,6 +1,5 @@
 exports.handler = async event => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+  
 
-  return {};
+  return { statusCode: 200, body: event };
 };
