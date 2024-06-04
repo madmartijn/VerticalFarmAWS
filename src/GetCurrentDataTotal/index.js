472eec9a2d7dd520650e5757d1f56a7cea8958ce
MARTIJN-DESKTOP\madma <mpmj.vanzelst@student.avans.nl>
2024-06-04 13:11:40 +0200

    Created all functions

--- src/GetCurrentDataTotal/index.js
+++ src/GetCurrentDataTotal/index.js
@@ -2,45 +2,62 @@ const {
   SecretsManagerClient,
   GetSecretValueCommand,
 } = require("@aws-sdk/client-secrets-manager");
+const mssql = require("mssql");
+
 const secret_name = "rds!db-08b378d2-fd52-4b72-a35a-53cdc01d922d";
 const client = new SecretsManagerClient({
   region: "eu-central-1",
 });
 
-const mssql = require("mssql");
+let pool;
 
-let response;
+async function getPool() {
+  if (pool) return pool; // if pool already exists, return it
+
+  const response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+
+  console.log(response);
+
+  const secret = JSON.parse(response.SecretString);
+
+  console.log(secret);
+
+  pool = new mssql.ConnectionPool({
+    user: secret.username,
+    password: secret.password,
+    port: MICROSOFTSQL_PORT,
+    database: "VerticalFarm",
+    server: MICROSOFTSQL_HOSTNAME,
+    options: {
+      encrypt: true, // Enable encryption for security
+      connectTimeout: 15000, // Set the connection timeout to 15 seconds
+    },
+  });
+
+  console.log(pool);
+
+  pool.on("error", (err) => {
+    console.error(err);
+    pool.close();
+    pool = null;
+  });
+
+  await pool.connect();
+
+  console.log("Connected to Microsoft SQL Server");
+
+  return pool;
+}
 
 exports.handler = async (event) => {
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
+  const pool = await getPool();
+  const data = await pool.query`SELECT * FROM dbo.DataHistorie`;
+  // no need to close the pool
 
-    const secret = response.SecretString;
-
-    const pool = new mssql.ConnectionPool({
-      user: secret.username,
-      password: secret.password,
-      port: 1433,
-      server: "database-1.cpgsme0uejc2.eu-central-1.rds.amazonaws.com",
-      options: {
-        encrypt: true, // Enable encryption for security
-      },
-    });
-
-    await pool.connect();
-
-    console.log('connected');
-
-    await pool.close();
-
-    return { statusCode: 200, body: "result" };
-  } catch (error) {
-    console.error(error);
-    return { statusCode: 500, body: JSON.stringify("Error") };
-  }
+  return { statusCode: 200, body: data };
 };
--- src/GetGewassen/index.js
+++ src/GetGewassen/index.js
@@ -1,6 +1,63 @@
-exports.handler = async event => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+const mssql = require("mssql");
 
-  return {};
+const secret_name = "rds!db-08b378d2-fd52-4b72-a35a-53cdc01d922d";
+const client = new SecretsManagerClient({
+  region: "eu-central-1",
+});
+
+let pool;
+
+async function getPool() {
+  if (pool) return pool; // if pool already exists, return it
+
+  const response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+
+  console.log(response);
+
+  const secret = JSON.parse(response.SecretString);
+
+  console.log(secret);
+
+  pool = new mssql.ConnectionPool({
+    user: secret.username,
+    password: secret.password,
+    port: MICROSOFTSQL_PORT,
+    database: "VerticalFarm",
+    server: MICROSOFTSQL_HOSTNAME,
+    options: {
+      encrypt: true, // Enable encryption for security
+      connectTimeout: 15000, // Set the connection timeout to 15 seconds
+    },
+  });
+
+  console.log(pool);
+
+  pool.on("error", (err) => {
+    console.error(err);
+    pool.close();
+    pool = null;
+  });
+
+  await pool.connect();
+
+  console.log("Connected to Microsoft SQL Server");
+
+  return pool;
+}
+
+exports.handler = async (event) => {
+  const pool = await getPool();
+  const data = await pool.query`SELECT * FROM dbo.Gewas WHERE LadeId = $event.pathParameters.LadeId`;
+  // no need to close the pool
+
+  return { statusCode: 200, body: data };
 };
