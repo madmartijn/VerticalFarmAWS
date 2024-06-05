a97556f710e504b5e4eb43894d48ae83f1ee28f4
MARTIJN-DESKTOP\madma <mpmj.vanzelst@student.avans.nl>
2024-06-05 10:38:45 +0200

    Fixed api calls

--- src/GetCurrentDataLade/index.js
+++ src/GetCurrentDataLade/index.js
@@ -1,45 +1,20 @@
-const {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} = require("@aws-sdk/client-secrets-manager");
 const mssql = require("mssql");
-
-const secret_name = "rds!db-08b378d2-fd52-4b72-a35a-53cdc01d922d";
-const client = new SecretsManagerClient({
-  region: "eu-central-1",
-});
-
 let pool;
 
 async function getPool() {
   if (pool) return pool; // if pool already exists, return it
 
-  const response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-
-  console.log(response);
-
-  const secret = JSON.parse(response.SecretString);
-
-  console.log(secret);
-
   pool = new mssql.ConnectionPool({
-    user: secret.username,
-    password: secret.password,
-    port: MICROSOFTSQL_PORT,
+    user: "admin",
+    password: "KY56]St4J[%>KADQmODERPlVScyd",
+    port: 1433,
     database: "VerticalFarm",
-    server: MICROSOFTSQL_HOSTNAME,
+    server: "database-1.cpgsme0uejc2.eu-central-1.rds.amazonaws.com",
     options: {
-      encrypt: true, // Enable encryption for security
+      encrypt: false, // Enable encryption for security
       connectTimeout: 15000, // Set the connection timeout to 15 seconds
     },
   });
-
-  console.log(pool);
 
   pool.on("error", (err) => {
     console.error(err);
@@ -49,15 +24,20 @@ async function getPool() {
 
   await pool.connect();
 
-  console.log("Connected to Microsoft SQL Server");
-
   return pool;
 }
 
 exports.handler = async (event) => {
-  const pool = await getPool();
-  const data = await pool.query`SELECT * FROM dbo.DataHistorie WHERE LadeId = $event.pathParameters.LadeId`;
-  // no need to close the pool
+  try {
+    const pool = await getPool();
 
-  return { statusCode: 200, body: data };
+    const data =
+      await pool.query`SELECT * FROM dbo.DataHistorie WHERE LadeId = ${event.pathParameters.LadeId.toString()}`;
+    // no need to close the pool
+
+    return { statusCode: 200, body: JSON.stringify(data.recordset) };
+  } catch (err) {
+    console.error(err);
+    return { statusCode: 500, body: "Internal Server Error" };
+  }
 };
--- src/GetCurrentDataTotal/index.js
+++ src/GetCurrentDataTotal/index.js
@@ -6,7 +6,7 @@ async function getPool() {
 
   pool = new mssql.ConnectionPool({
     user: "admin",
-    password: "O+qG5}||4$+g:9K1SF4+|MvWvMQM",
+    password: "KY56]St4J[%>KADQmODERPlVScyd",
     port: 1433,
     database: "VerticalFarm",
     server: "database-1.cpgsme0uejc2.eu-central-1.rds.amazonaws.com",
@@ -30,12 +30,13 @@ async function getPool() {
 exports.handler = async (event) => {
   try {
     const pool = await getPool();
+
     const data = await pool.query`SELECT * FROM dbo.DataHistorie`;
     // no need to close the pool
 
     return { statusCode: 200, body: JSON.stringify(data.recordset) };
   } catch (err) {
     console.error(err);
-    return { statusCode: 500, body: JSON.stringify(err) };
+    return { statusCode: 500, body: "Internal Server Error" };
   }
 };
